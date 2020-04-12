e189f581add1f850640ba03f7dfd42b5d4a27c3e
gtl <gtlgao@163.com>
2020-04-12 11:24:38 +0800

    add pirntln("

--- src/main/java/com/huawi/GitTest.java
+++ src/main/java/com/huawi/GitTest.java
@@ -1,5 +1,7 @@
 package com.huawi;
 
+import java.sql.SQLOutput;
+
 /**
  * @author: Gao Tonglin
  * @date: Create on 2019
@@ -7,5 +9,6 @@ package com.huawi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
     }
 }
