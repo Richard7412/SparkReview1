3cfbb323716b04cd89e77b827d5e8f6df7d5c8de
gtl <gtlgao@163.com>
2020-04-12 11:06:19 +0800

    new Project

---
+++ src/main/java/com/huawi/GitTest.java
@@ -0,0 +1,11 @@
+package com.huawi;
+
+/**
+ * @author: Gao Tonglin
+ * @date: Create on 2019
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
